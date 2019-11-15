86d1cb6a4057ef836106d101099c659a69f609ca
JosephJvB <joevanbo@pm.me>
2019-11-16 10:58:27 +1300

    add refresh calc

--- spotify_bot/main.py
+++ spotify_bot/main.py
@@ -1,21 +1,19 @@
 import os
 import base64
+import time
 from spotipy import Spotify, util
 import requests
 
 class Spotify_Bot(object):
     def __init__(self):
         self.auth = None
-        # self.get_playlist()
-        # self.get_oauth_token()
-        #self.refresh()
+        self.last_refresh = 0
+        self.refresh_auth()
         return
 
     def get_cc_token(self): # no scopes, basic auth
         u = 'https://accounts.spotify.com/api/token'
-        h = {
-            'Authorization': f'Basic {self.get_spotty_base64()}'
-        }
+        h = { 'Authorization': self.get_basic_auth() }
         d = { 'grant_type': 'client_credentials' }
         res = requests.post(u, data=d, headers=h)
         return res.json()['access_token']
@@ -36,40 +34,49 @@ class Spotify_Bot(object):
             'code': os.getenv('SPOTIFY_CODE'),
             'redirect_uri': 'https://jvb-spotty-auth.herokuapp.com/success'
         }
-        h = {
-            'Authorization': f'Basic {self.get_spotty_base64()}'
-        }
+        h = { 'Authorization': self.get_basic_auth() }
         u = 'https://accounts.spotify.com/api/token'
         res = requests.post(u, data=d, headers=h)
         print(res)
         print(res.json())
         print('DONE')
-        self.auth = res.json()
         return
 
     def get_me(self):
-        h = { 'Authorization': f"Bearer {os.getenv('SPOTIFY_ACCESS')}" }
+        h = { 'Authorization': f'Bearer {self.auth.access_token}' }
         u = 'https://api.spotify.com/v1/me/playlists'
         res = requests.get(u, headers=h)
         print(res)
         print(res.json())
         return
 
-    def refresh(self):
+    def refresh_auth(self):
         d = {
             'grant_type': 'refresh_token',
             'refresh_token': os.getenv('SPOTIFY_REFRESH')
         }
-        h = { 'Authorization': f'Basic {self.get_spotty_base64()}' }
+        h = { 'Authorization': self.get_basic_auth() }
         u = 'https://accounts.spotify.com/api/token'
         res = requests.post(u, data=d, headers=h)
         print(res)
         print(res.json())
+        self.auth = res.json()
+        self.last_refresh = time.time()
         return
 
-    def get_spotty_base64(self):
+    def need_refresh(self):
+        if not self.auth: return True
+        now = time.time()
+        if now - self.last_refresh > 3500:
+            return True
+        else:
+            return False
+
+
+    def get_basic_auth(self):
         s = f'{os.getenv("SPOTIFY_ID")}:{os.getenv("SPOTIFY_SECRET")}'
-        return str(base64.b64encode(s.encode('utf-8')), 'utf-8')
+        token = str(base64.b64encode(s.encode('utf-8')), 'utf-8')
+        return f'Basic {token}'
 
 
 if __name__ == '__main__':
