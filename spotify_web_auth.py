d1bcae82b1c46a10cd223e799038c5cf509cc9ff
JosephJvB <joevanbo@pm.me>
2019-11-16 10:01:12 +1300

    fix redirec uri encoded

--- spotify_web_auth.py
+++ spotify_web_auth.py
@@ -24,9 +24,9 @@ def init_spotty_auth():
 #   return render_template("index.html")
     scopes = 'playlist-read-private playlist-read-collaborative user-read-currently-playing user-read-private'
     url = ''.join([
-        'https://accounts.spotify.com/en/authorize?',
+        'https://accounts.spotify.com/authorize?',
         f'client_id={os.getenv("SPOTIFY_ID")}',
-        '&redirect_uri=https://jvb-spotty-auth.herokuapp.com/success',
+        '&redirect_uri=https%3A%2F%2Fjvb-spotty-auth.herokuapp.com%2Fsuccess',
         f'&scope={scopes}'
         '&response_type=code'
         # f'&state={state}', # jukebot had this??
