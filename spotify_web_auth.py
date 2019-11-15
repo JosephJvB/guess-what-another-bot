5b6f1f30da0debd8525730b7deac374287c11638
JosephJvB <joevanbo@pm.me>
2019-11-16 10:45:28 +1300

    oop

--- spotify_web_auth.py
+++ spotify_web_auth.py
@@ -36,7 +36,3 @@ def init_spotty_auth():
 if __name__ == '__main__':
     p = int(os.getenv('PORT', 3000))
     api.run(host='0.0.0.0', port=p)
-
-x = 'https://accounts.spotify.com/authorize?client_id=9e65a5b32a91465da947ea3621cdba37&redirect_uri=https%3A%2F%2Fjvb-spotty-auth.herokuapp.com%2Fsuccess&scope=playlist-read-private%20playlist-read-collaborative%20user-read-currently-playing%20user-read-private&response_type=code'
-
-y = 'https://accounts.spotify.com/en/login?continue=https:%2F%2Faccounts.spotify.com%2Fauthorize%3Fscope%3Dplaylist-read-private%2Bplaylist-read-collaborative%2Bplaylist-modify-private%2Bplaylist-modify-public%2Bstreaming%2Buser-read-currently-playing%2Buser-read-email%2Buser-read-private%2Buser-modify-playback-state%26response_type%3Dcode%26redirect_uri%3Dhttps%253A%252F%252Fgetjukebot.com%252Fspotify%252Fauth%26state%3D61gch1YLqYUeAbGQYoUqA3xI9idK3SzZeHgksuXq%26client_id%3Df5328ccd31d0450d85b25136db719464%26show_dialog%3D0'
