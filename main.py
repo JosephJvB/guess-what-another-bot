f4f593c05b0dd5761f3a810a3bb0ab0ee10103b5
JosephJvB <joevanbo@pm.me>
2019-11-17 11:54:26 +1300

    im spawning too many threads but game works

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import asyncio
 from slack_bot.main import Slack_Bot
 from spotify_bot.main import Spotify_Bot
 
+TIMEOUT = int(os.getenv('TIMEOUT'))
+
 class Game(object):
     def __init__(self):
         self.timer = None
@@ -20,7 +22,7 @@ class Game(object):
         print('START GAME IN')
         t = self.spotify.get_current_track()
         if t == None:
-            self.set_timeout(15, self.start_game)
+            self.set_timeout(15, self.start_game, 'start_game')
             print('START GAME OUT: NO SONG PLAYING')
             return
         self.start_round(t)
@@ -38,7 +40,7 @@ class Game(object):
         self.current_track = t
         self.answer_emoji = self.slack.get_emoji(ans_idx)
         self.slack.start_round_msg(track_options)
-        self.set_timeout(30, self.check_round_end)
+        self.set_timeout(TIMEOUT, self.check_round_end, 'check_round_end')
         print('START ROUND OUT')
         return
 
@@ -46,7 +48,7 @@ class Game(object):
         print('CHECK ROUND IN')
         t = self.spotify.get_current_track()
         if t == self.current_track:
-            self.set_timeout(15, self.check_round_end)
+            self.set_timeout(TIMEOUT, self.check_round_end, 'check_round_end')
             print('CHECK ROUND OUT: NO NEW SONG')
             return
         self.slack.post_end_msg(
@@ -56,27 +58,30 @@ class Game(object):
         print('CHECK ROUND OUT: NEW SONG, NEW ROUND')
         return
 
-    def set_timeout(self, t, fn):
-
+    def set_timeout(self, t, fn, n):
         if self.timer and self.thread:
             self.timer.cancel()
             self.thread.join()
+            self.timer = None
+            self.thread = None
         self.timer = Timer(t, fn)
-        self.thread = Thread(target=self.timer.start)
+        self.thread = Thread(target=self.timer.start, name=n)
         self.thread.start()
         return
 
-g = Game()
-t = Thread(target=g.start_game)
-t.start()
 
 @slack.RTMClient.run_on(event='message')
 def on_message(**payload):
     print('hi')
 
 l = asyncio.get_event_loop()
-c = slack.RTMClient(token=os.getenv('SLACK_TOKEN'))
+c = slack.RTMClient(token=os.getenv('SLACK_TOKEN'), loop=l)
+
+g = Game()
+t = Thread(target=g.start_game, daemon=True, name='init_game_thread')
+
 try:
+    t.start()
     l.run_forever(c.start())
 except KeyboardInterrupt:
     l.close()
