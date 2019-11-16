08ae7d7e69c194d9631e06c3ca8e2b3e439c8056
JosephJvB <joevanbo@pm.me>
2019-11-17 10:18:10 +1300

    dont set double timers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import random
-from threading import Timer
+from threading import Timer, Thread
 import slack
 import asyncio
 
@@ -9,7 +9,7 @@ from spotify_bot.main import Spotify_Bot
 
 class Game(object):
     def __init__(self):
-        self.thread = None
+        self.timer = None
         self.current_track = ''
         self.answer_emoji = ''
         self.spotify = Spotify_Bot()
@@ -20,12 +20,10 @@ class Game(object):
         print('START GAME IN')
         t = self.spotify.get_current_track()
         if t == None:
-            self.handle_thread(15, self.start_game)
+            self.set_timeout(15, self.start_game)
             print('START GAME OUT: NO SONG PLAYING')
             return
-
         self.start_round(t)
-        self.handle_thread(30, self.check_round_end)
         print('START GAME OUT')
         return
 
@@ -40,7 +38,7 @@ class Game(object):
         self.current_track = t
         self.answer_emoji = self.slack.get_emoji(ans_idx)
         self.slack.start_round_msg(track_options)
-        self.handle_thread(30, self.check_round_end)
+        self.set_timeout(30, self.check_round_end)
         print('START ROUND OUT')
         return
 
@@ -48,7 +46,7 @@ class Game(object):
         print('CHECK ROUND IN')
         t = self.spotify.get_current_track()
         if t == self.current_track:
-            self.handle_thread(15, self.check_round_end)
+            self.set_timeout(15, self.check_round_end)
             print('CHECK ROUND OUT: NO NEW SONG')
             return
         self.slack.post_end_msg(
@@ -58,20 +56,20 @@ class Game(object):
         print('CHECK ROUND OUT: NEW SONG, NEW ROUND')
         return
 
-    def handle_thread(self, t, fn):
-        if self.thread: self.thread.cancel()
-        self.thread = Timer(t, fn)
-        self.thread.start()
+    def set_timeout(self, t, fn):
+        if self.timer: self.timer.cancel()
+        self.timer = Timer(t, fn)
+        self.timer.start()
         return
 
 Game()
 
-# @slack.RTMClient.run_on(event='message')
-# async def on_message(**payload):
-#     return
+@slack.RTMClient.run_on(event='message')
+def on_message(**payload):
+    print('hi')
 # loop2 = asyncio.new_event_loop()
 # asyncio.set_event_loop(loop2)
-# rtm_client = slack.RTMClient(token=os.getenv('SLACK_TOKEN'), loop=loop2)
+# rtm_client = slack.RTMClient(token=os.getenv('SLACK_TOKEN'))
 # loop2.run_forever(rtm_client.start())
 
 # def on_msg_evt(self, payload):
